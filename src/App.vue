fea160cbad58e0f2ee8ea0c2f8f13171bcf7cd0c
lcruan <1416802331@qq.com>
2024-02-24 15:42:54 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  项目实战
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "mxg", // 这个base 提现在  http://localhost:5173/mxg
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
